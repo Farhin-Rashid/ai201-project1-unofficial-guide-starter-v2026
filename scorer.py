fe7640a23c166685713fc2c02c0f09a3e1faa4d2
Farhin-Rashid <123594031+Farhin-Rashid@users.noreply.github.com>
2026-09-23 20:44:34 -0500

    Fix indentation and parameter name in judge function

--- scorer.py
+++ scorer.py
@@ -1,4 +1,4 @@
-def judge(question: str, expects: str, answer: str, result) -> bool:
-  if not expects:
-    return False
-  return expects.strip().lower() in (answer or "".lower())
+def judge(question: str, expects: str, answer: str, results) -> bool:
+    if not expects:
+        return False        
+    return expects.strip().lower() in (answer or "".lower())
